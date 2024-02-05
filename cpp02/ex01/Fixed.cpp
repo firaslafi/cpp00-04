b8d7d6cd767f74f4e56d5de600f427623998e56b
Firas Lafi <flafi@3-B-8.42heilbronn.de>
2024-02-05 23:54:42 +0100

    ex01 done 03 added

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -5,14 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 22:21:52 by flafi             #+#    #+#             */
-/*   Updated: 2024/02/05 22:39:17 by flafi            ###   ########.fr       */
+/*   Created: 2024/02/05 22:31:51 by flafi             #+#    #+#             */
+/*   Updated: 2024/02/05 23:46:07 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// bit shifting calculator https://bit-calculator.com/bit-shift-calculator
 # include "./Fixed.hpp"
 
 using std::cout;
+using std::roundf;
+using std::ostream;
 
 Fixed::Fixed(void): _fpValue(0)
 {
@@ -34,7 +37,7 @@ Fixed& Fixed::operator=(const Fixed& source)
     cout << "Copy assignment operator called" << std::endl;
     if (this != &source)
     {
-        this->_fpValue = source.getRawBits();
+        this->_fpValue = source._fpValue;
     }
     return (*this);
 }
@@ -46,4 +49,29 @@ int Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
     this->_fpValue = raw;
+}
+// int constructor
+Fixed::Fixed(const int nbInt)
+{
+    cout << "Int constructor called" << std::endl;
+    this->_fpValue = (nbInt << this->_fractionalBits);
+    
+}
+// const float point number constructor
+Fixed::Fixed(const float nbFloat)
+{
+    cout << "Float constructor called" << std::endl;
+    this->_fpValue = (roundf(nbFloat * (1 << _fractionalBits)));
+}
+float   Fixed::toFloat( void ) const
+{
+    return ((float)_fpValue / (1 << _fractionalBits));
+}
+int Fixed::toInt( void ) const {
+    return (_fpValue >> _fractionalBits);
+}
+ostream &operator<<( std::ostream &outpudstream, Fixed const &num)
+{
+    outpudstream << num.toFloat();
+    return (outpudstream);
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -5,13 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 22:21:57 by flafi             #+#    #+#             */
-/*   Updated: 2024/02/05 22:39:22 by flafi            ###   ########.fr       */
+/*   Created: 2024/02/05 22:31:54 by flafi             #+#    #+#             */
+/*   Updated: 2024/02/05 23:45:06 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 # include <string>
+# include <cmath>
+# include <fstream>
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
@@ -26,7 +28,9 @@ class Fixed
     
     // copy constructor
     Fixed(const Fixed& source);
-
+    
+    Fixed(const int nbInt);
+    Fixed(const float nbFloat);
     
     // copy assignment operator overload
     Fixed& operator=(const Fixed& source);
@@ -34,10 +38,14 @@ class Fixed
     int getRawBits( void ) const;
     void setRawBits( int const raw );
     
+    float toFloat( void ) const;
+    int toInt( void ) const;
+    
     private:
     
     int _fpValue;
     static const int _fractionalBits = 8;
 };
 
+std::ostream &operator<<( std::ostream &outpudstream, Fixed const &num);
 #endif
